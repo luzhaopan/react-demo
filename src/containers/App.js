2664f457177023cc54122ffc44f9349ec5eb32bd
luzhaopan <745003249@qq.com>
2020-04-30 17:10:25 +0800

    updata

---
+++ src/containers/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import logo from "../public/logo.svg";
+// import "./App.css";
+
+function App() {
+  return <div className="App">首页</div>;
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./pages/App";
+// import "./index.css";
+import App from "../src/containers/App";
 import * as serviceWorker from "../src/services/serviceWorker";
 
 ReactDOM.render(
